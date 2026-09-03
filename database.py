85f7162cc7b7a8abf923b18914a0b8b41a8aaa3a
Mohamed Ali <mokhaledfathy2010@gmail.com>
2026-09-03 19:34:07 +0300

    Remove user-related table definitions
    Removed Users, Students, and Teachers table definitions from database.py.

--- database.py
+++ database.py
@@ -6,28 +6,4 @@ DATABASE_URL = "mysql+mysqlconnector://root:PASSWORD@localhost/NAME"
 Engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=Engine)
 Base = declarative_base()
-#Main table for users
-class Users(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nationalId = Column(String(14), unique=True,index=True )
-    name = Column(String(255))
-    email = Column(String(255), unique=True, index=True)
-    password = Column(String(255))
-    role = Column(String(10))
-#Student table
-class Students(Base):
-    __tablename__ = "students"
-
-    id = Column(Integer, primary_key=True, index=True)
-    userId = Column(Integer, ForeignKey("users.id"), unique=True, nullable=False)
-    grade = Column(String(50))
-#Teachers table
-class Teachers(Base):
-    __tablename__ = "teachers"
-
-    id = Column(Integer, primary_key=True, index=True)
-    userId = Column(Integer, ForeignKey("users.id"), unique=True, nullable=False)
-    subject = Column(String(255))
 
