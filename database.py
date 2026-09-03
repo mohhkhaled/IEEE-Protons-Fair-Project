2947b9dd5a71108c9f583bc7f4948d6c17191559
DARK-ELMASRY <mokhaledfathy2010@gmail.com>
2026-09-03 19:50:25 +0300

    Adding messages table

--- database.py
+++ database.py
@@ -18,3 +18,13 @@ class Users(Base):
     role = Column(String(10))
     school_id = Column(Integer, ForeignKey("schools.id"))
     created_at = Column(String(255))
+#Messages table for storing messages
+class Messages(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sender_id = Column(Integer, ForeignKey("users.id"))
+    receiver_id = Column(Integer, ForeignKey("users.id"))
+    content = Column(String(255))
+    sent_at = Column(String(255))
+    is_read = Column(Integer, default=0)  # 0 for unread, 1 for read
